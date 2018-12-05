51b5a77487b67e77d6da9db100033e76ef378507
unai.me <me@unai.me>
2018-12-05 23:18:42 +0100

    landing page in place

---
+++ resources/lang/en/fq.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'description' => 'FlowQuiz redefines online quizzes making the process stupidly quick and simple while creating spectacular results.',
+    'cards' => [
+        'find'   => 'Find Popular Quizzes',
+        'finddesc' => 'Don\'t now where to start? Check the list for the hottest quizzes out there. ',
+        'create'   => 'Create your own FlowQuiz',
+        'createdesc' => 'Try our supersimple FlowQuiz Creator. Sign in and be amazed with the amount of options you will find to transform your ideas into a FlowQuiz! ',
+        'compete'   => 'Challenge your friends!',
+        'competedesc' => 'Share a FlowQuiz with your friends and get back their results to have fun.',
+        'share'   => 'Share your results',
+        'sharedesc' => 'You can easily share your results on socialmedia.',
+    ],
+
+];
